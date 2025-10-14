eea74d63d68087b79595b3aa64eb79ad14d417a9
Suzad <61018495+Suzad@users.noreply.github.com>
2025-10-14 14:43:58 +0100

    update Cell class
    -use __post_init__ method so that if parameter is given, we can automatically get hmat, gmat from it and vice versa.

--- core/cell.py
+++ core/cell.py
@@ -5,6 +5,7 @@ from numpy.typing import NDArray
 from ase.cell import Cell as ASECell
 import numpy.linalg as la
 
+
 @dataclass
 class Cell:
     a:float = 0.0
@@ -17,10 +18,32 @@ class Cell:
     gmat: NDArray[np.float64] = field(default_factory=lambda: np.zeros((3, 3)))
 
 
-    def lp2box(self):
-        cell = ASECell.fromcellpar([self.a, self.b, self.c, self.alpha, self.beta, self.gamma])
-        self.hmat = cell.array
-        self.gmat = la.inv(self.hmat)
+    def __post_init__(self):
+        """
+        Initialization rules:
+        - If hmat has any non-zero entries: trust it and back-fill (a,b,c,alpha,beta,gamma).
+        - Else if a,b,c look set (>0): build hmat from these params.
+        - Else: leave uninitialized; conversions will raise until set.
+        """
+        # Prefer an explicitly provided matrix if it looks non-empty
+        if np.any(self.hmat):
+            params = ASECell(self.hmat).cellpar()
+            self.a, self.b, self.c, self.alpha, self.beta, self.gamma = params
+            self.gmat = la.inv(self.hmat)
+        # Otherwise, build from parameters if they look meaningful
+        elif (self.a > 0.0) or (self.b > 0.0) or (self.c > 0.0):
+            cell = ASECell.fromcellpar([self.a, self.b, self.c, self.alpha, self.beta, self.gamma])
+            self.hmat = cell.array
+            self.gmat = la.inv(self.hmat)
+        # else: keep zero matrices/params; user must call set_* before converting
 
-    def mat2box(self):
-        self.a, self.b, self.c, self.alpha, self.beta, self.gamma = ASECell(self.hmat).cellpar()
+
+    # def lp2box(self):
+    #     cell = ASECell.fromcellpar([self.a, self.b, self.c, self.alpha, self.beta, self.gamma])
+    #     self.hmat = cell.array
+    #     self.gmat = la.inv(self.hmat)
+    #
+    #
+    # def mat2box(self):
+    #     self.a, self.b, self.c, self.alpha, self.beta, self.gamma = ASECell(self.hmat).cellpar()
+
--- tests/test_cpAtom.py
+++ tests/test_cpAtom.py
@@ -1,18 +1,11 @@
+from numpy.linalg import det
+
 from core.atom import Atom
 from core.cell import Cell
-from crystools import typeatom
+from crystools import typeatom, cart2frac
 import numpy as np
 
-a=Cell(a=5, b=6, c=7, alpha=90, beta=100, gamma=120)
-a.lp2box()
-# print(a.hmat)
-# print(a.gmat)
 
-b = np.array([
-                 [5.,          0.,          0.],
-                 [-3.,          5.19615242,  0.],
-             [-1.21553724, - 0.70179075, 6.85783923],
-])
-c=Cell(hmat=b)
-c.mat2box()
-print(c.a,c.b,c.c,c.alpha,c.beta,c.gamma)
+obj=Cell(a=5,b=6,c=7,alpha=90,beta=100,gamma=120)
+print(obj.hmat)
+print(obj.gmat)
