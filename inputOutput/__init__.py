a54bdfb0eaa273757c0a8dc71c8f13434f5e4aca
Suzad <61018495+Suzad@users.noreply.github.com>
2026-01-01 07:13:54 +0000

    init file update

--- inputOutput/__init__.py
+++ inputOutput/__init__.py
@@ -1,11 +1,14 @@
 from .gro import readGro,writeGro
 from .itp import readItp
+from .pdb import readPdb,writePdb
 from .poscar import readPoscar,writePoscar
 
 __all__ = [
     "readGro",
     "writeGro",
     "readItp",
+    "readPdb",
+    "writePdb",
     "readPoscar",
     "writePoscar",
 ]
